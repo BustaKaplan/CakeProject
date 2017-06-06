5632cbb41fffa1bd25699f2aa75d9e7b74ee6ac3
BustaKaplan <chelsea.kaplan3@gmail.com>
2017-06-06 18:48:05 -0500

    added javascript

---
+++ assets/JavaScript/Custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $("#submit").click(function(){
+    var name = $('#name').val();
+  alert("Thank you for your order, "+ name +
+  "! We will get back to you shortly ")
+  })
+  $('#submit').click(function () {
+    $('#name').val('');
+
+});
+
+  });
--- assets/JavaScript/Menu.js
+++ assets/JavaScript/Menu.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  $(window).on('scroll', function(){
+    if ($(document).scrollTop() > 25) {
+    $('nav').addClass('shrink');
+    $('nav').addClass('nav-item-shrink');
+    $('nav').css('position', 'fixed');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+
+});
 
 $(".cakes").on('click', function(){
   $(".show-cakes").fadeIn();
